3dba1e255a5fe5809900dae452c4df292b435b3e
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-08-16 01:18:43 +0300

    adds start files

---
+++ src/components/App/App.js
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+
+// import s from "./App.module.css";
+
+class App extends Component {
+  // static defaultProps = {
+  //   initialValue: 0,
+  // };
+
+  state = {
+    // good: this.props.initialValue,
+    // neutral: this.props.initialValue,
+    // bad: this.props.initialValue,
+  };
+
+  render() {
+    // const onLeaveFeedback = this.onLeaveFeedback;
+    // const options = this.state;
+    // const { good, neutral, bad } = this.state;
+    // const total = this.countTotalFeedback();
+    // const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <>
+        <div className=""></div>
+      </>
+    );
+  }
+}
+
+export default App;
